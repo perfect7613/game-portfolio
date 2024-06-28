4d5391efd92d0519a0041d6396cdc3de702a70fa
Amey Muke <amitjoshi2052003@gmail.com>
2024-06-28 20:20:01 +0530

    Done

---
+++ src/constants.js
@@ -0,0 +1,22 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in React/NextJs these days.
+    I also have experience in Flutter and 3d Libraries such as ThreeJs and ReactThreeFiber as well. Besides this I also love Starbucks Coffee.
+    Here is my <a href="https://github.com/perfect7613" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
+  I also have my youtube channel will be putting more videos now as i have completed my degree. Go sub to <a href="https://www.youtube.com/@Coding_Wizardry" target="_blank">my channel</a>! (If you like the content)`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+  <a href="https://www.youtube.com/@SonnySangha" target="_blank">Sonny Sangha</a> (sometimes) and <a href="https://www.youtube.com/@harkirat1" target="_blank">Harkirat Singh</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1DCRRzA5dXVx5BLNpYF2gIqzcZ6lZu-sk/view?usp=drive_link" target="_blank">Check it out?</a>
+  Contact me at ameymuke252003@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+  Kaboom.js by reading this docs <a href="https://kaboomjs.com/" target="_blank">here</a>.`,
+  library: `I read some novels and harry potter sometimes.
+  I probably only read one of them. I'm not a big reader but I do like to read sometimes.`,
+  exit: `If you want to exit Amey's portfolio, just close the tab.`,
+};
